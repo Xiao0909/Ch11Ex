aaf14a5d855427d029168b9b5966eb483ea5b0da
xiaox2 <xiaox2@VDI-CECH-IC-01>
2022-04-04 17:56:42 -0400

    Add project files.

---
+++ TempManager/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Ch11Ex1TempManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TempManagerContext data { get; set; }
+        public HomeController(TempManagerContext ctx) => data = ctx;
+
+        public ViewResult Index()
+        {
+            var temps = data.Temps.OrderBy(t => t.Date).ToList();
+            return View(temps);
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View(new Temp());
+
+        [HttpPost]
+        public IActionResult Add(Temp temp)
+        {
+            var existing = data.Temps.Where(temp => temp.Date.Equals(temp.Date)).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                if(existing == null) { 
+                    data.Temps.Add(temp);
+                    data.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Date has already existed.");
+                }
+               
+            }
+            else
+            {
+                ModelState.AddModelError("", "There are errors in the form.");
+            }
+            return View(temp);
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            var temp = data.Temps.Find(id);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(Temp temp)
+        {
+            data.Remove(temp);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempManager.Models;
+
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller
+    {
+
+        private TempManagerContext data { get; set; }
+        public ValidationController(TempManagerContext ctx) => data = ctx;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CheckDate(string date)
+        {
+            var dateTime = DateTime.Parse(date);
+
+            var existing = data.Temps.Where(temp => temp.Date.Equals(dateTime)).FirstOrDefault();
+
+            if(existing == null)
+            {
+                return Json(true);
+            }
+
+            return Json("Date has already existed");
+        }
+    }
+}
